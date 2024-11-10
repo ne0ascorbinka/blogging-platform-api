8f2f448ecce9439cb640e7fef6d7a790b38b8fd0
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-11-10 20:29:13 +0200

    add model

--- blogging_platform_api/posts/models.py
+++ blogging_platform_api/posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="posts")
+    tags = models.ManyToManyField(Tag, related_name="posts")
+    created_at = models.TimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True, auto_now_add=False)
