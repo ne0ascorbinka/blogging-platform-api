429347c579beb33704e9c2c44b185602e3458604
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-11-11 14:42:59 +0200

    add term search

--- blogging_platform_api/posts/views.py
+++ blogging_platform_api/posts/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Post
 from .serializers import PostSerializer
+
+
+class TermSearchFilter(filters.SearchFilter):
+    search_param = 'term'
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [TermSearchFilter]
+    search_fields = ['title', 'content', 'category__name']
+
