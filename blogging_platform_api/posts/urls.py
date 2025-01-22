0e367c3ddc175cc33d3abc4f7b0b9fc398486ec7
ne0ascorbinka <ne.ascorbinka@gmail.com>
2025-01-22 03:19:07 +0200

    fix: add basename to url

--- blogging_platform_api/posts/urls.py
+++ blogging_platform_api/posts/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from .views import PostViewSet
 
-router = routers.DefaultRouter()
-router.register('posts', PostViewSet)
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='post')
 
-urlpatterns = [
-    path('', include(router.urls))
-]
+urlpatterns = router.urls
